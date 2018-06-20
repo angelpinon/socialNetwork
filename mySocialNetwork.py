25ccc2f56738ff2d15f49ce0935b21568d1e112e
angelpinon <40184493+angelpinon@users.noreply.github.com>
2018-06-20 11:51:56 -0700

    Add files via upload

--- mySocialNetwork.py
+++ mySocialNetwork.py
@@ -1,4 +1,6 @@
 ##social network##
+import sys
+import optparse
 
 class User:
     def __init__(self,userName):
@@ -9,7 +11,7 @@ class User:
         self.userId = ""
         self.friends = []
         self.posts = []
-
+    
     def addFriend(self, obj):
         self.friends.append(obj)
 
@@ -28,23 +30,44 @@ class User:
         for friend in self.friends:
             if friend.userName == obj.userName:
                 self.friends.remove(obj)
+
+    def viewMyProfile(self):
+        print(self.userName)
+        print("")
+        print("friends:")
+        self.viewFriends()
+        print("")
+        print("my posts:")
+        print(self.posts)
+        
         
 if __name__ == "__main__":
     
-    Angel = User("user_name")
+    Angel = User("intergalactor_")
     ham = User("THE_hamburgler")
     doe = User("$$$DoeDoppler$$$") 
 
     Angel.addFriend(ham)
     Angel.addFriend(doe)
 
-    ham.addPost("this is a post")
+    ham.addPost("this is library")
     doe.addPost("hellllloooo")
+    Angel.addPost("this isss myyyyy post")
+    Angel.addPost("this is another post. awesome")
 
-    Angel.viewFriends()
-    Angel.viewFeed()
-    
-    
-    
+    parser = optparse.OptionParser()
+parser.add_option('-n', '--name', dest='name', help='Your Name')
+
+(options, args) = parser.parse_args()
+
+if options.name is None:
+    options.name = input('profile? Or, newsfeed?:')
+
+if options.name == ("profile"):
+    sayOption = Angel.viewMyProfile()
+elif options.name == ("newsfeed"):
+    sayOption = Angel.viewFeed()
+##elif options.name == ("see friends"):
+##    sayOption = Angel.viewFeed()
     
     
